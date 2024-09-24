abe6af6e77b0c8687079508d1616959f3d73bb24
Aashish Patil <aashishp@google.com>
2024-09-23 17:47:19 -0700

    Adjust Visibility of API OperationRequest and other apis

--- Sources/MutationRef.swift
+++ Sources/MutationRef.swift
@@ -15,11 +15,11 @@
 import Foundation
 
 @available(iOS 15.0, macOS 11.0, tvOS 15.0, watchOS 8.0, *)
-public struct MutationRequest<Variable: OperationVariable>: OperationRequest {
-  public var operationName: String
-  public var variables: Variable?
+struct MutationRequest<Variable: OperationVariable>: OperationRequest {
+  private(set) var operationName: String
+  private(set) var variables: Variable?
 
-  public init(operationName: String, variables: Variable? = nil) {
+  init(operationName: String, variables: Variable? = nil) {
     self.operationName = operationName
     self.variables = variables
   }
@@ -27,7 +27,7 @@ public struct MutationRequest<Variable: OperationVariable>: OperationRequest {
 
 @available(iOS 15.0, macOS 11.0, tvOS 15.0, watchOS 8.0, *)
 public class MutationRef<ResultData: Decodable, Variable: OperationVariable>: OperationRef {
-  public var request: any OperationRequest
+  private var request: any OperationRequest
 
   private var grpcClient: GrpcClient
 
