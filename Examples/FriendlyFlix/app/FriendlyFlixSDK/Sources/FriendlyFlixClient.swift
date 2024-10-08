49c55066e5658fa10ae21f80f23dbdc0b6c68b0f
Peter Friese <peter@peterfriese.de>
2024-10-08 19:09:04 +0100

    🎨  Fix formating
    Signed-off-by: Peter Friese <peter@peterfriese.de>

--- Examples/FriendlyFlix/app/FriendlyFlixSDK/Sources/FriendlyFlixClient.swift
+++ Examples/FriendlyFlix/app/FriendlyFlixSDK/Sources/FriendlyFlixClient.swift
@@ -1,63 +1,60 @@
-import Foundation
 import FirebaseDataConnect
-
-
-
-
-
-
+import Foundation
 
 public extension DataConnect {
-
   static var friendlyFlixConnector: FriendlyFlixConnector = {
-    let dc = DataConnect.dataConnect(connectorConfig: FriendlyFlixConnector.connectorConfig, callerSDKType: .generated)
+    let dc = DataConnect.dataConnect(
+      connectorConfig: FriendlyFlixConnector.connectorConfig,
+      callerSDKType: .generated
+    )
     return FriendlyFlixConnector(dataConnect: dc)
   }()
-
 }
 
 public class FriendlyFlixConnector {
-
   var dataConnect: DataConnect
 
-  public static let connectorConfig = ConnectorConfig(serviceId: "friendly-flix-service", location: "us-central1", connector: "friendly-flix")
+  public static let connectorConfig = ConnectorConfig(
+    serviceId: "friendly-flix-service",
+    location: "us-central1",
+    connector: "friendly-flix"
+  )
 
   init(dataConnect: DataConnect) {
     self.dataConnect = dataConnect
 
-    // init operations 
-    self.upsertUserMutation = UpsertUserMutation(dataConnect: dataConnect)
-    self.addFavoritedMovieMutation = AddFavoritedMovieMutation(dataConnect: dataConnect)
-    self.deleteFavoritedMovieMutation = DeleteFavoritedMovieMutation(dataConnect: dataConnect)
-    self.addReviewMutation = AddReviewMutation(dataConnect: dataConnect)
-    self.updateReviewMutation = UpdateReviewMutation(dataConnect: dataConnect)
-    self.deleteReviewMutation = DeleteReviewMutation(dataConnect: dataConnect)
-    self.listMoviesQuery = ListMoviesQuery(dataConnect: dataConnect)
-    self.getMovieByIdQuery = GetMovieByIdQuery(dataConnect: dataConnect)
-    self.getActorByIdQuery = GetActorByIdQuery(dataConnect: dataConnect)
-    self.getCurrentUserQuery = GetCurrentUserQuery(dataConnect: dataConnect)
-    self.getIfFavoritedMovieQuery = GetIfFavoritedMovieQuery(dataConnect: dataConnect)
-    self.searchAllQuery = SearchAllQuery(dataConnect: dataConnect)
-    
+    // init operations
+    upsertUserMutation = UpsertUserMutation(dataConnect: dataConnect)
+    addFavoritedMovieMutation = AddFavoritedMovieMutation(dataConnect: dataConnect)
+    deleteFavoritedMovieMutation = DeleteFavoritedMovieMutation(dataConnect: dataConnect)
+    addReviewMutation = AddReviewMutation(dataConnect: dataConnect)
+    updateReviewMutation = UpdateReviewMutation(dataConnect: dataConnect)
+    deleteReviewMutation = DeleteReviewMutation(dataConnect: dataConnect)
+    listMoviesQuery = ListMoviesQuery(dataConnect: dataConnect)
+    getMovieByIdQuery = GetMovieByIdQuery(dataConnect: dataConnect)
+    getActorByIdQuery = GetActorByIdQuery(dataConnect: dataConnect)
+    getCurrentUserQuery = GetCurrentUserQuery(dataConnect: dataConnect)
+    getIfFavoritedMovieQuery = GetIfFavoritedMovieQuery(dataConnect: dataConnect)
+    searchAllQuery = SearchAllQuery(dataConnect: dataConnect)
   }
 
-  public func useEmulator(host: String = DataConnect.EmulatorDefaults.host, port: Int = DataConnect.EmulatorDefaults.port) {
-    self.dataConnect.useEmulator(host: host, port: port)
+  public func useEmulator(host: String = DataConnect.EmulatorDefaults.host,
+                          port: Int = DataConnect.EmulatorDefaults.port) {
+    dataConnect.useEmulator(host: host, port: port)
   }
 
   // MARK: Operations
-public let upsertUserMutation: UpsertUserMutation
-public let addFavoritedMovieMutation: AddFavoritedMovieMutation
-public let deleteFavoritedMovieMutation: DeleteFavoritedMovieMutation
-public let addReviewMutation: AddReviewMutation
-public let updateReviewMutation: UpdateReviewMutation
-public let deleteReviewMutation: DeleteReviewMutation
-public let listMoviesQuery: ListMoviesQuery
-public let getMovieByIdQuery: GetMovieByIdQuery
-public let getActorByIdQuery: GetActorByIdQuery
-public let getCurrentUserQuery: GetCurrentUserQuery
-public let getIfFavoritedMovieQuery: GetIfFavoritedMovieQuery
-public let searchAllQuery: SearchAllQuery
 
-
+  public let upsertUserMutation: UpsertUserMutation
+  public let addFavoritedMovieMutation: AddFavoritedMovieMutation
+  public let deleteFavoritedMovieMutation: DeleteFavoritedMovieMutation
+  public let addReviewMutation: AddReviewMutation
+  public let updateReviewMutation: UpdateReviewMutation
+  public let deleteReviewMutation: DeleteReviewMutation
+  public let listMoviesQuery: ListMoviesQuery
+  public let getMovieByIdQuery: GetMovieByIdQuery
+  public let getActorByIdQuery: GetActorByIdQuery
+  public let getCurrentUserQuery: GetCurrentUserQuery
+  public let getIfFavoritedMovieQuery: GetIfFavoritedMovieQuery
+  public let searchAllQuery: SearchAllQuery
 }
