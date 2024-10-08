2675a437f07f42d38d215bc4ac031487cdfbcdb5
Peter Friese <peter@peterfriese.de>
2024-10-08 19:33:53 +0100

    ♻️  Re-generate code
    Signed-off-by: Peter Friese <peter@peterfriese.de>

--- Examples/FriendlyFlix/app/FriendlyFlixSDK/Package.swift
+++ Examples/FriendlyFlix/app/FriendlyFlixSDK/Package.swift
@@ -4,30 +4,31 @@
 import PackageDescription
 
 let package = Package(
-  name: "FriendlyFlixSDK",
-  platforms: [
-    .iOS(.v17),
-    .macOS(.v14),
-    .watchOS(.v10),
-    .tvOS(.v17),
-  ],
-  products: [
-    .library(
-      name: "FriendlyFlixSDK",
-      targets: ["FriendlyFlixSDK"]
-    ),
-  ],
-  dependencies: [
-    .package(url: "https://github.com/firebase/data-connect-ios-sdk", from: "11.3.0-beta"),
-
-  ],
-  targets: [
-    .target(
-      name: "FriendlyFlixSDK",
-      dependencies: [
-        .product(name: "FirebaseDataConnect", package: "data-connect-ios-sdk"),
-      ],
-      path: "Sources"
-    ),
-  ]
+    name: "FriendlyFlixSDK",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+        .watchOS(.v10),
+        .tvOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "FriendlyFlixSDK",
+            targets: ["FriendlyFlixSDK"]),
+    ],
+    dependencies: [
+      
+        .package(url: "https://github.com/firebase/data-connect-ios-sdk", from: "11.3.0-beta"),
+      
+    ],
+    targets: [
+        .target(
+            name: "FriendlyFlixSDK",
+            dependencies: [
+              .product(name:"FirebaseDataConnect", package:"data-connect-ios-sdk")
+            ],
+            path: "Sources"
+        )
+    ]
 )
+
--- Examples/FriendlyFlix/app/FriendlyFlixSDK/Sources/FriendlyFlixKeys.swift
+++ Examples/FriendlyFlix/app/FriendlyFlixSDK/Sources/FriendlyFlixKeys.swift
@@ -2,324 +2,430 @@ import Foundation
 
 import FirebaseDataConnect
 
+
+
 public struct ActorKey {
+  
   public private(set) var id: UUID
+  
 
   enum CodingKeys: String, CodingKey {
-    case id
+    
+    case  id
+    
   }
 }
 
-extension ActorKey: Codable {
+extension ActorKey : Codable {
   public init(from decoder: any Decoder) throws {
     var container = try decoder.container(keyedBy: CodingKeys.self)
     let codecHelper = CodecHelper<CodingKeys>()
 
-    id = try codecHelper.decode(UUID.self, forKey: .id, container: &container)
+    
+    self.id = try codecHelper.decode(UUID.self, forKey: .id, container: &container)
+    
   }
 
   public func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    let codecHelper = CodecHelper<CodingKeys>()
-
-    try codecHelper.encode(id, forKey: .id, container: &container)
-  }
+      var container = encoder.container(keyedBy: CodingKeys.self)
+      let codecHelper = CodecHelper<CodingKeys>()
+      
+      
+      try codecHelper.encode(id, forKey: .id, container: &container)
+      
+      
+    }
 }
 
-extension ActorKey: Equatable {
+extension ActorKey : Equatable {
   public static func == (lhs: ActorKey, rhs: ActorKey) -> Bool {
+    
     if lhs.id != rhs.id {
       return false
     }
-
+    
     return true
   }
 }
 
-extension ActorKey: Hashable {
+extension ActorKey : Hashable {
   public func hash(into hasher: inout Hasher) {
-    hasher.combine(id)
+    
+    hasher.combine(self.id)
+    
   }
 }
+
+
 
 public struct FavoriteMovieKey {
+  
   public private(set) var userId: String
-
+  
   public private(set) var movieId: UUID
+  
 
   enum CodingKeys: String, CodingKey {
-    case userId
-
-    case movieId
+    
+    case  userId
+    
+    case  movieId
+    
   }
 }
 
-extension FavoriteMovieKey: Codable {
+extension FavoriteMovieKey : Codable {
   public init(from decoder: any Decoder) throws {
     var container = try decoder.container(keyedBy: CodingKeys.self)
     let codecHelper = CodecHelper<CodingKeys>()
 
-    userId = try codecHelper.decode(String.self, forKey: .userId, container: &container)
-
-    movieId = try codecHelper.decode(UUID.self, forKey: .movieId, container: &container)
+    
+    self.userId = try codecHelper.decode(String.self, forKey: .userId, container: &container)
+    
+    self.movieId = try codecHelper.decode(UUID.self, forKey: .movieId, container: &container)
+    
   }
 
   public func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    let codecHelper = CodecHelper<CodingKeys>()
-
-    try codecHelper.encode(userId, forKey: .userId, container: &container)
-
-    try codecHelper.encode(movieId, forKey: .movieId, container: &container)
-  }
+      var container = encoder.container(keyedBy: CodingKeys.self)
+      let codecHelper = CodecHelper<CodingKeys>()
+      
+      
+      try codecHelper.encode(userId, forKey: .userId, container: &container)
+      
+      
+      
+      try codecHelper.encode(movieId, forKey: .movieId, container: &container)
+      
+      
+    }
 }
 
-extension FavoriteMovieKey: Equatable {
+extension FavoriteMovieKey : Equatable {
   public static func == (lhs: FavoriteMovieKey, rhs: FavoriteMovieKey) -> Bool {
+    
     if lhs.userId != rhs.userId {
       return false
     }
-
+    
     if lhs.movieId != rhs.movieId {
       return false
     }
-
+    
     return true
   }
 }
 
-extension FavoriteMovieKey: Hashable {
+extension FavoriteMovieKey : Hashable {
   public func hash(into hasher: inout Hasher) {
-    hasher.combine(userId)
-
-    hasher.combine(movieId)
+    
+    hasher.combine(self.userId)
+    
+    hasher.combine(self.movieId)
+    
   }
 }
+
+
 
 public struct MovieActorKey {
+  
   public private(set) var movieId: UUID
-
+  
   public private(set) var actorId: UUID
+  
 
   enum CodingKeys: String, CodingKey {
-    case movieId
-
-    case actorId
+    
+    case  movieId
+    
+    case  actorId
+    
   }
 }
 
-extension MovieActorKey: Codable {
+extension MovieActorKey : Codable {
   public init(from decoder: any Decoder) throws {
     var container = try decoder.container(keyedBy: CodingKeys.self)
     let codecHelper = CodecHelper<CodingKeys>()
 
-    movieId = try codecHelper.decode(UUID.self, forKey: .movieId, container: &container)
-
-    actorId = try codecHelper.decode(UUID.self, forKey: .actorId, container: &container)
+    
+    self.movieId = try codecHelper.decode(UUID.self, forKey: .movieId, container: &container)
+    
+    self.actorId = try codecHelper.decode(UUID.self, forKey: .actorId, container: &container)
+    
   }
 
   public func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    let codecHelper = CodecHelper<CodingKeys>()
-
-    try codecHelper.encode(movieId, forKey: .movieId, container: &container)
-
-    try codecHelper.encode(actorId, forKey: .actorId, container: &container)
-  }
+      var container = encoder.container(keyedBy: CodingKeys.self)
+      let codecHelper = CodecHelper<CodingKeys>()
+      
+      
+      try codecHelper.encode(movieId, forKey: .movieId, container: &container)
+      
+      
+      
+      try codecHelper.encode(actorId, forKey: .actorId, container: &container)
+      
+      
+    }
 }
 
-extension MovieActorKey: Equatable {
+extension MovieActorKey : Equatable {
   public static func == (lhs: MovieActorKey, rhs: MovieActorKey) -> Bool {
+    
     if lhs.movieId != rhs.movieId {
       return false
     }
-
+    
     if lhs.actorId != rhs.actorId {
       return false
     }
-
+    
     return true
   }
 }
 
-extension MovieActorKey: Hashable {
+extension MovieActorKey : Hashable {
   public func hash(into hasher: inout Hasher) {
-    hasher.combine(movieId)
-
-    hasher.combine(actorId)
+    
+    hasher.combine(self.movieId)
+    
+    hasher.combine(self.actorId)
+    
   }
 }
+
+
 
 public struct MovieMetadataKey {
+  
   public private(set) var id: UUID
+  
 
   enum CodingKeys: String, CodingKey {
-    case id
+    
+    case  id
+    
   }
 }
 
-extension MovieMetadataKey: Codable {
+extension MovieMetadataKey : Codable {
   public init(from decoder: any Decoder) throws {
     var container = try decoder.container(keyedBy: CodingKeys.self)
     let codecHelper = CodecHelper<CodingKeys>()
 
-    id = try codecHelper.decode(UUID.self, forKey: .id, container: &container)
+    
+    self.id = try codecHelper.decode(UUID.self, forKey: .id, container: &container)
+    
   }
 
   public func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    let codecHelper = CodecHelper<CodingKeys>()
-
-    try codecHelper.encode(id, forKey: .id, container: &container)
-  }
+      var container = encoder.container(keyedBy: CodingKeys.self)
+      let codecHelper = CodecHelper<CodingKeys>()
+      
+      
+      try codecHelper.encode(id, forKey: .id, container: &container)
+      
+      
+    }
 }
 
-extension MovieMetadataKey: Equatable {
+extension MovieMetadataKey : Equatable {
   public static func == (lhs: MovieMetadataKey, rhs: MovieMetadataKey) -> Bool {
+    
     if lhs.id != rhs.id {
       return false
     }
-
+    
     return true
   }
 }
 
-extension MovieMetadataKey: Hashable {
+extension MovieMetadataKey : Hashable {
   public func hash(into hasher: inout Hasher) {
-    hasher.combine(id)
+    
+    hasher.combine(self.id)
+    
   }
 }
+
+
 
 public struct MovieKey {
+  
   public private(set) var id: UUID
+  
 
   enum CodingKeys: String, CodingKey {
-    case id
+    
+    case  id
+    
   }
 }
 
-extension MovieKey: Codable {
+extension MovieKey : Codable {
   public init(from decoder: any Decoder) throws {
     var container = try decoder.container(keyedBy: CodingKeys.self)
     let codecHelper = CodecHelper<CodingKeys>()
 
-    id = try codecHelper.decode(UUID.self, forKey: .id, container: &container)
+    
+    self.id = try codecHelper.decode(UUID.self, forKey: .id, container: &container)
+    
   }
 
   public func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    let codecHelper = CodecHelper<CodingKeys>()
-
-    try codecHelper.encode(id, forKey: .id, container: &container)
-  }
+      var container = encoder.container(keyedBy: CodingKeys.self)
+      let codecHelper = CodecHelper<CodingKeys>()
+      
+      
+      try codecHelper.encode(id, forKey: .id, container: &container)
+      
+      
+    }
 }
 
-extension MovieKey: Equatable {
+extension MovieKey : Equatable {
   public static func == (lhs: MovieKey, rhs: MovieKey) -> Bool {
+    
     if lhs.id != rhs.id {
       return false
     }
-
+    
     return true
   }
 }
 
-extension MovieKey: Hashable {
+extension MovieKey : Hashable {
   public func hash(into hasher: inout Hasher) {
-    hasher.combine(id)
+    
+    hasher.combine(self.id)
+    
   }
 }
+
+
 
 public struct ReviewKey {
+  
   public private(set) var userId: String
-
+  
   public private(set) var movieId: UUID
+  
 
   enum CodingKeys: String, CodingKey {
-    case userId
-
-    case movieId
+    
+    case  userId
+    
+    case  movieId
+    
   }
 }
 
-extension ReviewKey: Codable {
+extension ReviewKey : Codable {
   public init(from decoder: any Decoder) throws {
     var container = try decoder.container(keyedBy: CodingKeys.self)
     let codecHelper = CodecHelper<CodingKeys>()
 
-    userId = try codecHelper.decode(String.self, forKey: .userId, container: &container)
-
-    movieId = try codecHelper.decode(UUID.self, forKey: .movieId, container: &container)
+    
+    self.userId = try codecHelper.decode(String.self, forKey: .userId, container: &container)
+    
+    self.movieId = try codecHelper.decode(UUID.self, forKey: .movieId, container: &container)
+    
   }
 
   public func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    let codecHelper = CodecHelper<CodingKeys>()
-
-    try codecHelper.encode(userId, forKey: .userId, container: &container)
-
-    try codecHelper.encode(movieId, forKey: .movieId, container: &container)
-  }
+      var container = encoder.container(keyedBy: CodingKeys.self)
+      let codecHelper = CodecHelper<CodingKeys>()
+      
+      
+      try codecHelper.encode(userId, forKey: .userId, container: &container)
+      
+      
+      
+      try codecHelper.encode(movieId, forKey: .movieId, container: &container)
+      
+      
+    }
 }
 
-extension ReviewKey: Equatable {
+extension ReviewKey : Equatable {
   public static func == (lhs: ReviewKey, rhs: ReviewKey) -> Bool {
+    
     if lhs.userId != rhs.userId {
       return false
     }
-
+    
     if lhs.movieId != rhs.movieId {
       return false
     }
-
+    
     return true
   }
 }
 
-extension ReviewKey: Hashable {
+extension ReviewKey : Hashable {
   public func hash(into hasher: inout Hasher) {
-    hasher.combine(userId)
-
-    hasher.combine(movieId)
+    
+    hasher.combine(self.userId)
+    
+    hasher.combine(self.movieId)
+    
   }
 }
+
+
 
 public struct UserKey {
+  
   public private(set) var id: String
+  
 
   enum CodingKeys: String, CodingKey {
-    case id
+    
+    case  id
+    
   }
 }
 
-extension UserKey: Codable {
+extension UserKey : Codable {
   public init(from decoder: any Decoder) throws {
     var container = try decoder.container(keyedBy: CodingKeys.self)
     let codecHelper = CodecHelper<CodingKeys>()
 
-    id = try codecHelper.decode(String.self, forKey: .id, container: &container)
+    
+    self.id = try codecHelper.decode(String.self, forKey: .id, container: &container)
+    
   }
 
   public func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    let codecHelper = CodecHelper<CodingKeys>()
-
-    try codecHelper.encode(id, forKey: .id, container: &container)
-  }
+      var container = encoder.container(keyedBy: CodingKeys.self)
+      let codecHelper = CodecHelper<CodingKeys>()
+      
+      
+      try codecHelper.encode(id, forKey: .id, container: &container)
+      
+      
+    }
 }
 
-extension UserKey: Equatable {
+extension UserKey : Equatable {
   public static func == (lhs: UserKey, rhs: UserKey) -> Bool {
+    
     if lhs.id != rhs.id {
       return false
     }
-
+    
     return true
   }
 }
 
-extension UserKey: Hashable {
+extension UserKey : Hashable {
   public func hash(into hasher: inout Hasher) {
-    hasher.combine(id)
+    
+    hasher.combine(self.id)
+    
   }
 }
+
+
