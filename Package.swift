c2cae7643b054e91600a5b393dfb24d3967cde7a
Aashish <112133849+aashishpatil-g@users.noreply.github.com>
2024-11-26 10:31:01 -0800

    Use AppCheckInterop APIs (#39)

--- Package.swift
+++ Package.swift
@@ -45,9 +45,10 @@ let package = Package(
       dependencies: [
         .product(name: "GRPC", package: "grpc-swift"),
         .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
-        // TODO: Investigate switching Auth and AppCheck to interop.
+
+        // TODO: Add FirebaseAppCheckInterop as a dependency once its available as an external library
+        // TODO: Investigate switching Auth to interop.
         .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
-        .product(name: "FirebaseAppCheck", package: "firebase-ios-sdk"),
         .product(name: "GULEnvironment", package: "GoogleUtilities"),
       ],
       path: "Sources"
