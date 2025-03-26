72c1ac1155e736b22a9174e303c5d1407f1bfbbb
Aashish Patil <aashishp@google.com>
2025-03-26 07:49:58 -0700

    Make boxed error accessible

--- Sources/DataConnectError.swift
+++ Sources/DataConnectError.swift
@@ -37,7 +37,7 @@ public extension DataConnectError {
 /// Type erased DataConnectError
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 public struct AnyDataConnectError: DataConnectError {
-  private let dataConnectError: DataConnectError
+  public let dataConnectError: DataConnectError
 
   init<E: DataConnectError>(dataConnectError: E) {
     self.dataConnectError = dataConnectError
