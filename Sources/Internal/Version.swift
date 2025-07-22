010632f25f11bf00587bf7b97106445ebab24511
Aashish <112133849+aashishpatil-g@users.noreply.github.com>
2025-07-22 13:45:55 -0700

    Update Sources/Internal/Version.swift
    Co-authored-by: Nick Cooke <36927374+ncooke3@users.noreply.github.com>

--- Sources/Internal/Version.swift
+++ Sources/Internal/Version.swift
@@ -18,7 +18,7 @@ import GoogleUtilities_Environment
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 struct Version {
-  static let sdkVersion = "11.7.3"
+  static let sdkVersion = "11.8.0"
 
   // returns value of form gl-PLATFORM_NAME/PLATFORM_VERSION
   static func platformVersionHeader() -> String {
